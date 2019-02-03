0b27dc7c9d1ef2f4e7131a3d4b763c8181504e5b
Wenhao Zhang <wenhaozhang@s-164-67-229-167.resnet.ucla.edu>
2019-02-03 00:43:33 -0800

    push dist

--- dict/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='Dict',
-    version='0.0.1 dev',
-    packages=['dict',],
-    license="A Merriam-Webster's Collegiate" + u'\N{REGISTERED SIGN}' + " Dictionary app sits in your CLI environment.",
-    long_description=open('README.md').read(),
-)
