359a7b96ddd2053333f9893c0a5afcc658abaa31
Wenhao Zhang <wenhaozhang@wifi-131-179-5-82.host.ucla.edu>
2019-02-03 16:54:47 -0800

    release 0.0.2

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mwdict",
+    version="0.0.2",
+    author="Wenhao Zhang",
+    author_email="wenhaoz@ucla.edu",
+    description="A Merriam-Webster dictionary app sits in your CLI environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jiaowoshabi/dict",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['mwdict=mwdict.command:main'],
+    }
+)
