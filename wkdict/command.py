1ef4cdf11d736b83a6595d5ac706d01b32716be9
Wenhao Zhang <wenhaoz@g.ucla.edu>
2019-05-22 00:02:36 -0700

    use click.utils to replace the ugly stmts & add commands options

--- wkdict/command.py
+++ wkdict/command.py
@@ -7,7 +7,14 @@ from wkdict.wkdict import parseJSON
 
 @click.command()
 @click.argument('word')
-def main(word="test"):
-    print("(* This application is powered by Wiktionary's API. *)")
+@click.option('--defs', is_flag=False, 
+    help="Maximum number of definition entries to print out.")
+@click.option('--egs', is_flag=False, 
+    help="Maximum number of example entries to print out.")
+@click.version_option()
+def main(word="love", defs="3", egs="3"):
+    click.secho("🔅 This application is powered by Wiktionary's API 🔅", fg='yellow')
+    click.echo()
     word, resjson = requestAPI(word)
-    parseJSON(word, resjson)
+    parseJSON(word, resjson, int(defs), int(egs))
+    click.echo()
--- wkdict/wkdict.py
+++ wkdict/wkdict.py
@@ -8,69 +8,89 @@ import textwrap
 from wkdict.constants import color
 from wiktionaryparser import WiktionaryParser
 
-def requestAPI(word="test"):
+def requestAPI(word="love"):
     """ Query wiktionary API for json results using WiktionaryParser
     Args:
-      word: queried word; default value, "test".
+        word: queried word; default value, "test".
     Returns:
-      word queried, and JSON table of queried word
+        word queried, and JSON table of queried word
     """    
     parser = WiktionaryParser()
     word_json = parser.fetch(word)
 
     return word, word_json
 
-def parseJSON(word, table):
-    """ Parse the json table based on 
+def parseJSON(word, table, defs, egs):
+    """ Parse the json table using library 
         https://github.com/Suyash458/WiktionaryParser
     Args:
-      table: returned json results on queried word from merriam-webster API.
+        word: string; target word we are looking up
+        table: json; returned json results on queried word from Wikitionary API.
+        defs: int; Maximum number of definition entries to print out.
+        egs: int; Maximum number of example entries to print out.
+
     Raises:
-      TypeError: If table is not a string type.
-      ValueError: If returned table is empty.
+        TypeError: If table is not a string type.
+        ValueError: If returned table is empty.
+
     Returns:
-      Print the definitions, pronunciations, partOfSpeech, use examples, related words, and etymology. (v 0.0.3)
+        Print the definitions, pronunciations, partOfSpeech, use examples, related words, and etymology.
     """
     res = list()
     if len(table[0]["definitions"]) < 1:
-        print(color.RED + 'Empty look-up result returned' + color.END)
+        click.secho("Sorry, I☹ can't find this word in the dictionary...", 
+                        fg='red', bold=True)
+        click.secho('🚨Please check the spelling or try a different word🚨', 
+                        fg='red', blink=True)
     else:
         for e_idx, entry in enumerate(table):
-            # Print out prons
+            # Print out word + prons (optional)
             pron_dict = entry.get("pronunciations", None)
-            if pron_dict is None:
-                print(str(e_idx+1) + ") " + color.BOLD + word + color.END)
-            else:
-                # get prons list
+            word_idx = str(e_idx+1) + ") "
+            pron = ""
+
+            if pron_dict is not None:
                 prons = pron_dict.get("text", [""])
-                pron = ""
                 if len(prons) != 0:
                     pron = prons[0]
-                print(str(e_idx+1) + ") " + color.BOLD + word + color.END + '\t' + color.PURPLE + pron + color.END)
+
+            click.secho(word_idx + word, bold=True, nl=False)
+            click.secho('\t' + pron, fg='bright_magenta')
 
             # Print out defitions
             definitions = entry.get("definitions", [])
             for def_idx, def_text in enumerate(definitions):
-                print('\t' + color.YELLOW + str(def_idx+1) + " " + def_text.get("partOfSpeech", "") + color.END)
-                print('\t' + color.UNDERLINE + 'Definitions' + color.END)
+                # print out part of speech
+                part_of_speech = def_text.get("partOfSpeech", "")
+                click.secho('   🏷  ' + part_of_speech, fg='yellow')
+                # print out underlined heading "definition"
+                click.echo()
+                click.echo('\t📗 ' + click.style('Definitions', underline=True))
+
                 for text_id, text in enumerate(def_text.get("text", [])):
-                    print('\t' +  color.GREEN + "-. "   + str_in_list(textwrap.wrap(text, width=80)) + color.END)
-
+                    # print out definition entries
+                    if text_id < defs:
+                        def_entry = str_in_list(textwrap.wrap(text, width=80))
+                        click.echo('\t☞ ' + click.style(def_entry, fg="green"))
+                click.echo()
                 if def_text.get("examples") != []:
-                  print('\t' + color.UNDERLINE + 'Examples' + color.END)
-                  for text_id, text in enumerate(def_text.get("examples", [])):
-                      print('\t'  +  color.BLUE + "// "   + str_in_list(textwrap.wrap(text, width=80)) + color.END)
-
+                    click.echo('\t📘 ' + click.style('Examples', underline=True))
+                    # print('\t' + color.UNDERLINE + 'Examples' + color.END)
+                    for text_id, text in enumerate(def_text.get("examples", [])):
+                        if text_id < egs:
+                            eg_entry = str_in_list(textwrap.wrap(text, width=80))
+                            click.echo('\t➡ ' + click.style(eg_entry, fg="blue"))
+                click.echo()
 
 def str_in_list(lst):
     """Concatenate all strings in a list of strings. For example,
        this function returns "Hello World!" from ["Hello", "World!"].
     Args:
-      lst: A list contains all strings to be concatenated.
+        lst: A list contains all strings to be concatenated.
     Raises:
-      TypeError: If lst is not a string type.
+        TypeError: If lst is not a string type.
     Returns:
-      The concatenated string.
+        The concatenated string.
     """
     assert isinstance(lst, list)
 
