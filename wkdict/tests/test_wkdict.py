f033de80f628f15a45889e5272a677ed83c9ec38
Wenhao Zhang <wenhaoz@g.ucla.edu>
2019-05-22 23:32:39 -0700

    add test cases

---
+++ wkdict/tests/test_wkdict.py
@@ -0,0 +1,29 @@
+import unittest
+from wkdict.wkdict import parseJSON
+from wkdict.wkdict import requestAPI
+
+class TestWkdict(unittest.TestCase):
+    def test_good_inputs(self):
+        raised = False
+        try:
+            words = ["If", "there", "a", "main", "concern", "Google",
+                     "may", "have", "oversold"]
+            for word in words[0]:
+                word, resjson = requestAPI(word)
+                ret = parseJSON(word, resjson, 1)
+        except:
+            raised = True
+        self.assertFalse(raised)
+
+    def test_bad_inputs(self):
+        raised = False
+        try:
+            words = [".", "?", "(Google)",
+                     "ma$y", "ha^ve"]
+            for word in words[0]:
+                word, resjson = requestAPI(word)
+                ret = parseJSON(word, resjson, 1)
+                self.assertTrue("The word you typed is" in ret)
+        except:
+            raised = True
+        self.assertFalse(raised)
